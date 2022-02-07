332f5f700bc17cad27ad2669b8f79810e1a5591c
Amiya Ashutosh Sahu <79083019+Amiya-10@users.noreply.github.com>
2022-02-07 16:56:27 +0530

    Add files via upload

---
+++ Dele1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates1
+{
+    class Dele1
+    {
+        private delegate double Operation(double num1, double num2);
+        static void Main(string[] args)
+        {
+            Console.Write("Input number 1: ");
+            var num1 = double.Parse(Console.ReadLine());
+            Console.Write("Input number 2: ");
+            var num2 = double.Parse(Console.ReadLine());
+            Console.Write("Input operation (+,*,/,- or max): ");
+            var option = Console.ReadLine().Trim();
+            Operation operation;
+            switch(option)
+            {
+                case "+":
+                    operation = ArithmeticOperations.Add;
+                    break;
+                case "*":
+                    operation = ArithmeticOperations.Multiply;
+                    break;
+                case "/":
+                    operation = ArithmeticOperations.Divide;
+                    break;
+                case "-":
+                    operation = ArithmeticOperations.Substract;
+                    break;
+                case "max":
+                    operation = ArithmeticOperations.Max;
+                    break;
+                default:
+                    Console.WriteLine("Error!!");
+                    return;
+            }
+            var result = operation(num1, num2);
+            Console.WriteLine($"Result: {result}");
+            Console.ReadKey();
+        }
+    }
+    public class ArithmeticOperations
+    {
+        public static double Add(double num1, double num2) => num1 + num2;
+        public static double Multiply(double num1, double num2) => num1 * num2;
+        public static double Divide(double num1, double num2) => num1 / num2;
+        public static double Substract(double num1, double num2) => num1 - num2;
+        public static double Max(double num1, double num2) => num1 >= num2 ? num1 : num2;
+    }
+}
---
+++ Dele2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates1
+{
+    class Dele2
+    {
+        delegate int MyDelegate(int num1, int num2);
+        static void Main(string[] args)
+        {
+            MyDelegate arOperation = (int num1, int num2) => num1 + num2;
+            PerformArithmeticOperation(1, 2, arOperation);
+            Console.ReadKey();
+        }
+        static void PerformArithmeticOperation(int n1, int n2, MyDelegate arOperation)
+        {
+            var result = arOperation(n1, n2);
+            Console.WriteLine(result);
+        }
+    }
+}
